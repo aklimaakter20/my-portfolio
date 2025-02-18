70bb848720c60692115db12b3bd641b92d9e8a16
aklimaakter20 <aklimaakter20@gmail.com>
2025-02-18 18:08:50 +0600

    image resize

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
   return (
     <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
       <div>
-        <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
         </div>
+        <img src="aklima.png" alt="" className='w-24'/>
             <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 '>Hi,I'm Aklima Akter <Image src={assets.hand_icon} alt='' className='w-6'/></h3>
             <h1 className='text-3xl sm:text-6xl lg:text-[66px]'>Frontend Web Developer based in Bangladesh</h1>
             <p className='max-w-2xl mx-auto '>I am a frontend developer from Bangladesh, with 1 year of experience in multiple companies like .............</p>
