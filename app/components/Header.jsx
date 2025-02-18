be1e1a975381050c74240cecb0299d26818e0d9c
aklimaakter20 <aklimaakter20@gmail.com>
2025-02-18 18:32:25 +0600

    image size decresing

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
       <div>
-      <img src="aklima.png" alt="" className='w-24 rounded-full'/>
+      <img src="aklima.png" alt="" className='w-4 rounded-full'/>
 
         </div>
             <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 '>Hi,I'm Aklima Akter <Image src={assets.hand_icon} alt='' className='w-6'/></h3>
